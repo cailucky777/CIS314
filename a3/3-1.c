ee0b945e7f65ff403eb8dcb79bb25c087e465605
cailucky777 <52936556+cailucky777@users.noreply.github.com>
2020-10-21 13:49:56 -0700

    A3

---
+++ a3/3-1.c
@@ -0,0 +1,106 @@
+//Luying Cai A3 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct IntArray {
+	int length;
+	int *dataPtr;
+};
+
+struct IntArray* mallocIntArray(int length){
+	// build a new pointer to access members 
+	struct IntArray *newptr;
+	// allocating memory for number of struct IntArray.
+	newptr = (struct IntArray *)(malloc(sizeof(struct IntArray)));
+	// allocating memory for int type dataPtr.
+	newptr->dataPtr = (int *)malloc(length * sizeof(int));
+	//assign length value to structure length.
+	newptr->length = length;
+	//return new pointer
+	return newptr
+
+}
+
+void freeIntArray(struct IntArray *arrayPtr){
+	//free the instence and free the instance's dataPtr.
+	
+	free(*arrayPtr->dataPtr);
+	*arrayPtr->length = 0;
+	free(arrayPtr);
+	
+}
+//cite: fgets:https://www.programiz.com/c-programming/c-strings
+//cite: strtol: https://www.tutorialspoint.com/c_standard_library/c_function_strtol.htm
+void readIntArray(struct IntArray *array){
+	//initialize:
+	char input[BUFSIZ];
+	char *ptr;
+	int integer;
+	//prompts and reads positive ints from the user.
+	printf("Enter positive number: "\n);
+	//read one int for each array index
+	for(int i = 0; i < array->length; i++){
+		fgets(input, sizeof(input), stdin);// get input from stdin/
+		integer = strtol(input, &ptr, 10);//change char to decimal assign to int integer.
+		if (integer <= 0)//if integer less or equal to 0, give error message.
+			printf(" Error: give me a positive number.\n");
+		else
+			array->dataPtr[i] = integer; 
+	}
+}
+
+//cite:https://www.programiz.com/dsa/selection-sort
+void swap(int *xp, int *yp){
+	//swap xp and yp through a cash.(from slide)
+	int t0 = *xp; 
+	int t1 = *yp;
+	*xp = t1;
+	*yp = t0;
+}
+// selection sort the input array in ascending order.
+void sortIntArray(struct IntArray *array){
+	//reduce the array length one by each iteration.
+	for (int i == 0; i < array->length - 1; i++){
+		int min = i;// store the minimum element, assume the first element is minimum.
+		for (int j = i+1; j < array->length; j++){ 
+			if (array->dataPtr[j] < array->dataPtr[min])] //find the minimum element.
+				min = j; //update minimum element.
+		}
+	swap(&array->dataPtr[min], &array->dataPtr[i]); // swap to keep ascending order.
+	}
+}
+
+void printIntArray(struct IntArray *array){
+	printf("[ "); 
+	for(int i = 0; i < array->length; i++){ //when i < array's length, print each element in array.
+		printf(" %d, ", array->dataPtr[i]);
+		if ( i != array->length - 1) // if not last one, print , after each elemnt.
+			printf(" , "); 
+		else
+			printf(" ]\n"); // last print ]. 
+	}
+}
+
+int main() {
+	char input[BUFSIZ];
+	char *ptr;
+	int integer;
+	printf("Enter length: ");//prompt the user to input a positive (>0) int length from the user
+	fgets(input, sizeof(BUFSIZ), stdin); 
+	integer = strtol(input, &ptr, 10);
+	while (integer <= 0){
+		printf("Please input a positive(>0) number.\n");// print error message if not positive number.
+		printf("Enter length: ");
+		fgets(input, sizeof(BUFSIZ), stdin); //again if input valid number
+		integer = strtol(input, &ptr, 10);
+	}
+	
+	struct IntArray* newArray = mallocIntArray(number);
+	readIntArray(newArray);
+	sortIntArray(newArray);
+	printIntArray(newArray);
+	freeIntArray(newArray);
+	
+	
+}
---
+++ a3/3-2.c
@@ -0,0 +1,36 @@
+//Luying Cai A3-2
+
+#include <stdio.h>
+
+long f(long x, long y, long z)
+{	// Because x is in %rdi, y is in %rsi, z is in %rdx, and that %rax is used to hold the return value.
+	// for convienve, 
+	long rdi = x; //Argument x
+	long rsi = y; //Argument y
+	long rdx = z; //Argument z
+	long rax; // return value
+	
+	//addq %rsi, %rdx, add rsi to rdx.
+	rdx = rsi + rdx;
+	//imulq %rdx, %rdi, multiple rdx to rdi.
+	rdi = rdx * rdi;
+	//salq $63, %rdx , shift rdx to left 63 bits.
+	rdx = rdx << 63;
+	//sarq $63, %rdx, shift rdx to right 63 bits.
+	rdx = rdx >> 63;
+	//movq %rdi, %rax, assign rdi value to rax.
+	rax = rdi;
+	//xorq %rdx, %rax, do xor. 
+	rax = rax ^ rdx;
+	
+	return rax; //return value. 
+}
+
+
+
+int main() {
+	printf("%ld\n", f(1, 2, 4)); //6
+	printf("%ld\n", f(3, 5, 7)); //36
+	printf("%ld\n", f(10, 20, 30)); //500
+	return 0;
+}
